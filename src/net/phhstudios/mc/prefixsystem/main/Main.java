27db90fcda8e652e7da2f6a1d8a507dabe80a4e4
someone <someone@someplace.com>
2021-05-11 21:16:43 +0200

    some init msg

---
+++ src/net/phhstudios/mc/prefixsystem/main/Main.java
@@ -0,0 +1,21 @@
+package net.phhstudios.mc.prefixsystem.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+        super.onDisable();
+    }
+}
