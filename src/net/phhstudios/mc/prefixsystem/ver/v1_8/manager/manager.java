716ebde0b1ac09e89d8a5fe98060ac42da84d675
someone <someone@someplace.com>
2021-05-12 05:47:02 +0200

    some init msg

---
+++ src/net/phhstudios/mc/prefixsystem/ver/v1_8/manager/manager.java
@@ -0,0 +1,28 @@
+package net.phhstudios.mc.prefixsystem.ver.v1_8.manager;
+
+public class manager
+{
+
+    public static void loadsystem()
+    {
+        loadcommands();
+        loadevents();
+        loadlisteners();
+    }
+
+    public static void loadcommands()
+    {
+
+    }
+
+    public static void loadevents()
+    {
+
+    }
+
+    public static void loadlisteners()
+    {
+
+    }
+
+}
